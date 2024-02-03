213b59628c33fd5cfb2a43e5c122cb05c0426769
omerasher2 <157235083+omerasher2@users.noreply.github.com>
2024-02-03 19:24:37 +0200

    AMEN

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -73,10 +73,10 @@ public class OneOfEachStats {
 		System.out.println("Number of families with 3 children: "+count3);
 		System.out.println("Number of families with 4 or more children: "+count4);
 		if (count2>count3 && count2>count4)
-		System.out.println("the most common number of children is 2.");
+		System.out.println("The most common number of children is 2.");
 		else if (count3>count2 && count3>count4)
-		System.out.println("the most common number of children is 3.");
+		System.out.println("The most common number of children is 3.");
 		else
-		System.out.println("the most common number of children is 4 or more.");
+		System.out.println("The most common number of children is 4 or more.");
 	}
 }
