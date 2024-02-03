7738494ca5852c2916142d4dcb4c4c5572323ed9
omerasher2 <157235083+omerasher2@users.noreply.github.com>
2024-02-03 16:57:09 +0200

    fix - divisor

--- Divisors.java
+++ Divisors.java
@@ -5,7 +5,7 @@ public class Divisors {
 	public static void main (String[] args) {
 		int num, divisor;
         num = Integer.parseInt(args[0]);
-        divisor = Integer.parseInt(args[1]);
+        divisor = 1;
         while (num+1 != divisor)
         {
             if (num%divisor == 0)
